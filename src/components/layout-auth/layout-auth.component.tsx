63b5f66bb625c5de4af06719020e0734185b4816
Denis Rodionov <zunh@protonmail.com>
2020-01-06 16:01:59 +0200

    Routing layouts

---
+++ src/components/layout-auth/layout-auth.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./layout-auth.module.scss";
+
+const LayoutAuth: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Auth Layout</h1>
+    </div>
+  );
+};
+
+export default LayoutAuth;
---
+++ src/components/layout-main/layout-main.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./layout-main.module.scss";
+
+const LayoutMain: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Main Layout</h1>
+    </div>
+  );
+};
+
+export default LayoutMain;
