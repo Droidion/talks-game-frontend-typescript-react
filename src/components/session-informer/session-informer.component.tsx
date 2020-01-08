fa652c49494baf3920eb00c6f9e254f388e98e11
Denis Rodionov <zunh@protonmail.com>
2020-01-08 02:38:39 +0200

    Redirect on sign out

--- src/components/session-informer/session-informer.component.tsx
+++ src/components/session-informer/session-informer.component.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 import { RootState } from "../../redux/root-reducer";
-import ISessionState from "../../types/ISessionState";
+import { emptySession } from "../../redux/session/session.actions";
 import styles from "./session-informer.module.scss";
 
-const SessionInformer: React.FC<ISessionState> = ({ session }) => {
+const SessionInformer: React.FC<ConnectedProps<typeof connector>> = ({
+  session,
+  emptySession,
+}) => {
   const { t } = useTranslation();
-  return (
+  return session ? (
     <div className={styles.wrapper}>
       <div>
-        {t("Team")} {session?.team_number}
+        {t("Team")} {session.team_number}, {t("Token")} {session.token}
       </div>
-      <div>
-        {t("Token")} {session?.token}
-      </div>
+      <button onClick={emptySession}>{t("Sign out")}</button>
     </div>
+  ) : (
+    <Redirect to="/auth/signin" />
   );
 };
 
@@ -24,4 +28,10 @@ const mapStateToProps = (state: RootState) => ({
   session: state.session.session,
 });
 
-export default connect(mapStateToProps)(SessionInformer);
+const mapDispatchToProps = {
+  emptySession: () => emptySession(),
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export default connector(SessionInformer);
