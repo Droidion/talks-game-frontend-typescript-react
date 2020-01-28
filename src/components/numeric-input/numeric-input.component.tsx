17e22d5d158646adabbec8a259c2bb62956c2f50
malevka <formaleva@gmail.com>
2020-01-29 00:54:31 +0300

    Add defaultProps, SetExtreme method, clapm method comments props

--- src/components/numeric-input/numeric-input.component.tsx
+++ src/components/numeric-input/numeric-input.component.tsx
@@ -5,57 +5,107 @@ import React from "react";
 import formatNumbersAsCurrency from "../../lib/formatNumbersAsCurrency";
 import styles from "./numeric-input.module.scss";
 
-interface INumericInputProps {
-  handleChange: Function;
-  incrementalAmount: Decimal;
-  initialValue: Decimal;
-  isExtremeHelpersActive: boolean;
-  isDisabled: boolean;
-  isIncrementerActive: boolean;
-  maxPossibleValue?: Decimal | undefined;
-  minPossibleValue?: Decimal | undefined;
-}
-
 interface INumericInputState {
+  /** Current inputted value */
   value: string;
 }
 
-type Props = INumericInputProps;
+interface INumericInputProps {
+  /** Event handler when user change input value */
+  handleChange: Function;
+  /** Amount for increase/decrease buttons */
+  incrementalAmount: Decimal;
+  /** Initial value of input */
+  initialValue: Decimal;
+  /** Should input be disabled */
+  isDisabled: boolean;
+  /** Should min/max helper buttons be shown */
+  isExtremeHelpersActive: boolean;
+  /** Should increase/decrease helper buttons be shown */
+  isIncrementerActive: boolean;
+  /** Max possible value */
+  maxPossibleValue: Decimal;
+  /** Min possible value */
+  minPossibleValue: Decimal;
+}
+
 /**
- * Numeric imput
+ * Numeric input with incremental and extreme stepper buttons. Input used only to enter numbers
  *
- * @visibleName NumericImput
+ * @visibleName NumericInput
  */
 
-class NumericImput extends React.Component<Props, INumericInputState> {
+class NumericInput extends React.Component<INumericInputProps, INumericInputState> {
   constructor(props: INumericInputProps) {
     super(props);
     this.state = {
-      value: formatNumbersAsCurrency(this.props.initialValue.toFixed(2)),
+      value: this.props.initialValue.toFixed(2),
     };
   }
 
-  normalizedValue = (value: string) => {
+  static defaultProps = {
+    maxPossibleValue: new Decimal(0),
+    minPossibleValue: new Decimal(0),
+  };
+
+  clamp = (number: number) => {
+    const { minPossibleValue } = this.props;
+    const { maxPossibleValue } = this.props;
+    return Decimal.min(
+      Decimal.max(number, minPossibleValue.toNumber()),
+      maxPossibleValue.toNumber()
+    );
+  };
+
+  handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = this.normalizeValue(event.target.value);
+    console.log(inputValue);
+    if (inputValue.match(/(^(\d+(\.?\d*)?)?$)/)) {
+      const calculatedValue = this.clamp(new Decimal(inputValue).toNumber());
+      console.log(calculatedValue.toString());
+      this.setState({ value: calculatedValue.toFixed(2) });
+
+      if (event.target.selectionStart) {
+        console.log(event.target.selectionStart);
+        event.target.setSelectionRange(
+          event.target.selectionStart,
+          event.target.selectionStart
+        );
+      }
+
+      this.props.handleChange(calculatedValue);
+    }
+  };
+
+  handleIncrement = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const increment =
+      event.currentTarget.name === "increase"
+        ? this.props.incrementalAmount
+        : this.props.incrementalAmount.neg();
+    const currentValue = this.normalizeValue(this.state.value);
+    const calculatedValue = this.clamp(
+      new Decimal(currentValue).add(increment).toNumber()
+    );
+    this.setState({ value: calculatedValue.toFixed(2) });
+    this.props.handleChange(calculatedValue);
+  };
+
+  normalizeValue = (value: string) => {
     const normalizedValue = value.replace(/\s/g, "");
     return !!normalizedValue ? normalizedValue : "0";
   };
-  handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = this.normalizedValue(event.target.value);
-    if (inputValue.match(/(^(\d+(\.?\d{0,2})?)?$)/)) {
-      this.setState({ value: formatNumbersAsCurrency(inputValue) });
-      this.props.handleChange(new Decimal(inputValue));
-      console.log(inputValue);
+
+  setExtreme = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name);
+    const extremeType = event.currentTarget.name;
+    const calculatedValue =
+      extremeType === "max"
+        ? this.props.maxPossibleValue
+        : this.props.minPossibleValue;
+    if (calculatedValue) {
+      this.setState({ value: calculatedValue.toFixed(2) });
+      this.props.handleChange(calculatedValue);
     }
-  };
-  handleIncrement = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const increment =
-      event.currentTarget.value === "increase"
-        ? this.props.incrementalAmount
-        : this.props.incrementalAmount.times(-1);
-    const currentValue = this.normalizedValue(this.state.value);
-    const changedValue = new Decimal(currentValue).add(increment).toString();
-    this.setState({ value: formatNumbersAsCurrency(changedValue) });
-    this.props.handleChange(changedValue);
   };
 
   render() {
@@ -67,14 +117,14 @@ class NumericImput extends React.Component<Props, INumericInputState> {
       >
         <button
           className={styles.incrementer}
-          value={"increase"}
+          name={"increase"}
           onClick={this.handleIncrement}
         >
           ▲
         </button>
         <button
           className={styles.incrementer}
-          value={"decrease"}
+          name={"decrease"}
           onClick={this.handleIncrement}
         >
           ▼
@@ -83,8 +133,12 @@ class NumericImput extends React.Component<Props, INumericInputState> {
     );
     const extremeHelpers = this.props.isExtremeHelpersActive && (
       <div className={classnames(styles.helpers, styles.outside)}>
-        <button className={styles.extreme}>max</button>
-        <button className={styles.extreme}>min</button>
+        <button className={styles.extreme} name={"max"} onClick={this.setExtreme}>
+          max
+        </button>
+        <button className={styles.extreme} name={"min"} onClick={this.setExtreme}>
+          min
+        </button>
       </div>
     );
     return (
@@ -103,4 +157,4 @@ class NumericImput extends React.Component<Props, INumericInputState> {
   }
 }
 
-export default NumericImput;
+export default NumericInput;
